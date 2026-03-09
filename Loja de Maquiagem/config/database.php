275f54295f8345d2a82cbb774d1c394a5ba7b4f1
Janine Cunha <janinecunha213@gmail.com>
2026-03-09 10:22:43 -0300

    Delete Loja de Maquiagem directory

--- Loja de Maquiagem/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Configurações do banco de dados
-return [
-    'host' => 'localhost',
-    'dbname' => 'loja_maquiagem',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4'
-];
